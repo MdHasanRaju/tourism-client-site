f2a92d791dc0e59c007ac4cb5f9767ed9612b249
MdHasanRaju <mdhasanraju2017@gmail.com>
2022-01-24 13:02:09 +0600

    changed

--- src/components/NewSession/NewSession.js
+++ src/components/NewSession/NewSession.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React,{useState} from "react";
 import "./NewSession.css";
 import CountUp from "react-countup";
 import VisibilitySensor from "react-visibility-sensor";
 
 const NewSession = () => {
-  const [focus, setFocus] = React.useState(false);
+  const [focus, setFocus] = useState(false);
+  
 
   return (
     <div className="session">
@@ -13,80 +14,133 @@ const NewSession = () => {
         <div className="d-flex justify-content-evenly align-items-center">
           <div>
             <h2 className="fs-1 text-light">
-            <CountUp start={focus ? 0 : null} end={30} duration={2} redraw={true}>
-            {({ countUpRef }) => (
-              <div>
+              {/* <CountUp
+                start={focus ? 0 : null}
+                end={48}
+                duration={2}
+                redraw={true}
+              >
+                {({ countUpRef }) => (
+                  <div>
+                    <span ref={countUpRef} />
+                    <VisibilitySensor
+                      onChange={(isVisible) => {
+                        if (isVisible) {
+                          setFocus(true);
+                        }
+                      }}
+                    >
+                      <a></a>
+                    </VisibilitySensor>
+                  </div>
+                )}
+              </CountUp> */}
+ <CountUp end={47} redraw={true}>
+        {({ countUpRef, start }) => (
+            <VisibilitySensor onChange={start} delayedCall>
                 <span ref={countUpRef} />
-                <VisibilitySensor
-                  onChange={isVisible => {
-                    if (isVisible) {
-                      setFocus(true);
-                    } 
-                  }}
-                >
-                  <a></a>
-                </VisibilitySensor>
-              </div>
-            )}
-          </CountUp>
+            </VisibilitySensor>
+        )}
+    </CountUp>
+              
             </h2>
             <p className="text-danger">Days</p>
           </div>
           <div>
-            <h2 className="fs-1 text-light"><CountUp start={focus ? 0 : null} end={45} duration={2} redraw={true}>
-            {({ countUpRef }) => (
-              <div>
+            <h2 className="fs-1 text-light">
+              {/* <CountUp
+                start={focus ? 0 : null}
+                end={45}
+                duration={2}
+                redraw={true}
+              >
+                {({ countUpRef }) => (
+                  <div>
+                    <span ref={countUpRef} />
+                    <VisibilitySensor
+                      onChange={(isVisible) => {
+                        if (isVisible) {
+                          setFocus(true);
+                        }
+                      }}
+                    >
+                      <a></a>
+                    </VisibilitySensor>
+                  </div>
+                )}
+              </CountUp> */} <CountUp end={35} redraw={true}>
+        {({ countUpRef, start }) => (
+            <VisibilitySensor onChange={start} delayedCall>
                 <span ref={countUpRef} />
-                <VisibilitySensor
-                  onChange={isVisible => {
-                    if (isVisible) {
-                      setFocus(true);
-                    } 
-                  }}
-                >
-                  <a></a>
-                </VisibilitySensor>
-              </div>
-            )}
-          </CountUp></h2>
+            </VisibilitySensor>
+        )}
+    </CountUp>
+            </h2>
             <p className="text-danger">Hours</p>
           </div>
           <div>
-            <h2 className="fs-1 text-light"><CountUp start={focus ? 0 : null} end={48} duration={2} redraw={true}>
-            {({ countUpRef }) => (
-              <div>
+            <h2 className="fs-1 text-light">
+              {/* <CountUp
+                start={focus ? 0 : null}
+                end={48}
+                duration={2}
+                redraw={true}
+              >
+                {({ countUpRef }) => (
+                  <div>
+                    <span ref={countUpRef} />
+                    <VisibilitySensor
+                      onChange={(isVisible) => {
+                        if (isVisible) {
+                          setFocus(true);
+                        }
+                      }}
+                    >
+                      <a></a>
+                    </VisibilitySensor>
+                  </div>
+                )}
+              </CountUp> */} <CountUp end={27} redraw={true}>
+        {({ countUpRef, start }) => (
+            <VisibilitySensor onChange={start} delayedCall>
                 <span ref={countUpRef} />
-                <VisibilitySensor
-                  onChange={isVisible => {
-                    if (isVisible) {
-                      setFocus(true);
-                    } 
-                  }}
-                >
-                  <a></a>
-                </VisibilitySensor>
-              </div>
-            )}
-          </CountUp></h2>
+            </VisibilitySensor>
+        )}
+    </CountUp>
+            </h2>
             <p className="text-danger">Minutes</p>
           </div>
           <div>
-            <h2 className="fs-1 text-light"><CountUp start={focus ? 0 : null} end={59} duration={2} redraw={true}>
-            {({ countUpRef }) => (
-              <div>
+            <h2 className="fs-1 text-light">
+              {/* <CountUp
+                start={focus ? 0 : null}
+                end={59}
+                duration={2}
+                redraw={true}
+              >
+                {({ countUpRef }) => (
+                  <div>
+                    <span ref={countUpRef} />
+                    <VisibilitySensor
+                      onChange={(isVisible) => {
+                        if (isVisible) {
+                          setFocus(true);
+                        }
+                      }}
+                    >
+                      <a></a>
+                    </VisibilitySensor>
+                  </div>
+                )}
+              </CountUp> */}
+               <CountUp end={59} redraw={true}>
+        {({ countUpRef, start }) => (
+            <VisibilitySensor onChange={start} delayedCall>
                 <span ref={countUpRef} />
-                <VisibilitySensor
-                  onChange={isVisible => {
-                    if (isVisible) {
-                      setFocus(true);
-                    } 
-                  }}
-                >
-                  <a></a>
-                </VisibilitySensor>
-              </div>
-            )}
-          </CountUp></h2>
+            </VisibilitySensor>
+        )}
+    </CountUp>
+            </h2>
             <p className="text-danger">Seconds</p>
           </div>
         </div>
