0534787c3d46fbf53a284062e33a2fb6c5d77ec1
MdHasanRaju <mdhasanraju2017@gmail.com>
2022-01-24 10:07:10 +0600

    react counter added

--- src/components/NewSession/NewSession.js
+++ src/components/NewSession/NewSession.js
@@ -1,32 +1,98 @@
-import React from 'react';
-import './NewSession.css';
+import React from "react";
+import "./NewSession.css";
+import CountUp from "react-countup";
+import VisibilitySensor from "react-visibility-sensor";
 
 const NewSession = () => {
-    return (
-      <div className="session">
-        <div className="container text-center">
-          <h2 className="text-warning mb-5">NEW ADVENTURES BEGIN IN</h2>
-          <div className="d-flex justify-content-evenly align-items-center">
-            <div>
-              <h2 className="fs-1 text-light">00</h2>
-              <p className="text-danger">Days</p>
-            </div>
-            <div>
-              <h2 className="fs-1 text-light">00</h2>
-              <p className="text-danger">Hours</p>
-            </div>
-            <div>
-              <h2 className="fs-1 text-light">00</h2>
-              <p className="text-danger">Minutes</p>
-            </div>
-            <div>
-              <h2 className="fs-1 text-light">00</h2>
-              <p className="text-danger">Seconds</p>
-            </div>
+  const [focus, setFocus] = React.useState(false);
+
+  return (
+    <div className="session">
+      <div className="container text-center">
+        <h2 className="text-warning mb-5">NEW ADVENTURES BEGIN IN</h2>
+        <div className="d-flex justify-content-evenly align-items-center">
+          <div>
+            <h2 className="fs-1 text-light">
+            <CountUp start={focus ? 0 : null} end={30} duration={2} redraw={true}>
+            {({ countUpRef }) => (
+              <div>
+                <span ref={countUpRef} />
+                <VisibilitySensor
+                  onChange={isVisible => {
+                    if (isVisible) {
+                      setFocus(true);
+                    } 
+                  }}
+                >
+                  <a></a>
+                </VisibilitySensor>
+              </div>
+            )}
+          </CountUp>
+            </h2>
+            <p className="text-danger">Days</p>
+          </div>
+          <div>
+            <h2 className="fs-1 text-light"><CountUp start={focus ? 0 : null} end={45} duration={2} redraw={true}>
+            {({ countUpRef }) => (
+              <div>
+                <span ref={countUpRef} />
+                <VisibilitySensor
+                  onChange={isVisible => {
+                    if (isVisible) {
+                      setFocus(true);
+                    } 
+                  }}
+                >
+                  <a></a>
+                </VisibilitySensor>
+              </div>
+            )}
+          </CountUp></h2>
+            <p className="text-danger">Hours</p>
+          </div>
+          <div>
+            <h2 className="fs-1 text-light"><CountUp start={focus ? 0 : null} end={48} duration={2} redraw={true}>
+            {({ countUpRef }) => (
+              <div>
+                <span ref={countUpRef} />
+                <VisibilitySensor
+                  onChange={isVisible => {
+                    if (isVisible) {
+                      setFocus(true);
+                    } 
+                  }}
+                >
+                  <a></a>
+                </VisibilitySensor>
+              </div>
+            )}
+          </CountUp></h2>
+            <p className="text-danger">Minutes</p>
+          </div>
+          <div>
+            <h2 className="fs-1 text-light"><CountUp start={focus ? 0 : null} end={59} duration={2} redraw={true}>
+            {({ countUpRef }) => (
+              <div>
+                <span ref={countUpRef} />
+                <VisibilitySensor
+                  onChange={isVisible => {
+                    if (isVisible) {
+                      setFocus(true);
+                    } 
+                  }}
+                >
+                  <a></a>
+                </VisibilitySensor>
+              </div>
+            )}
+          </CountUp></h2>
+            <p className="text-danger">Seconds</p>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default NewSession;
